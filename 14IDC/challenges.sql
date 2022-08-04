316f777f2681469a38c279aeeb5cd6e5ac4e0380
Andrew Michaelis <amichaelis70@s.tooeletech.edu>
2022-08-04 21:43:29 +0000

    15

--- 14IDC/challenges.sql
+++ 14IDC/challenges.sql
@@ -22,3 +22,39 @@ SELECT username
 FROM users LEFT JOIN photos
     ON users.id = photos.user_id
 WHERE photos.id IS NULL;
+
+# Challenge Four: Identify the user with the individual photo which has the most likes
+SELECT 
+	username, 
+    COUNT(*) AS likes,
+    photos.id AS photo_id
+FROM photos JOIN likes
+    ON photos.id = likes.photo_id
+JOIN users
+    ON photos.user_id = users.id
+GROUP BY photos.id
+ORDER BY likes DESC
+LIMIT 1;
+
+# Challenge Five: Find how many times a user posts on average, avg photos per user
+SELECT
+    (SELECT COUNT(*) FROM photos) / (SELECT COUNT(*) FROM users) AS "AVG";
+    
+# Challenge Six: What are the top 5 most commonly used hashtags?
+SELECT
+    tags.tag_name,
+    COUNT(*) AS uses
+FROM tags JOIN photo_tags
+    ON tags.id = photo_tags.tag_id
+GROUP BY tags.id
+ORDER BY uses DESC
+LIMIT 5;
+
+# Challenge Seven: A problem with bots, find users who have liked every photo on the site
+SELECT 
+	username, 
+	Count(*) AS num_likes 
+FROM users INNER JOIN likes 
+	ON users.id = likes.user_id 
+GROUP  BY likes.user_id 
+HAVING num_likes = (SELECT Count(*) FROM   photos);
